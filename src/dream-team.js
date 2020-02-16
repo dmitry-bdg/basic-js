54afcaf4f26359aa046cf7ee8cf42ea168f54203
Dmitry Balahonov <balahonov.dmitry@gmail.com>
2020-02-16 17:18:20 +0300

    feat: add solution dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    let array = [];
+    let array2 = [];
+    for (let i in members) {
+        if (typeof members[i] === 'string') {
+          array.push(members[i].trim()[0]);
+        } else {
+          array2.push(members[i]);
+        }
+    }
+    return array.sort().join("").toLocaleUpperCase();
 };
