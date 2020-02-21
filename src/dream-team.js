6c8c3dd667cbaf898fde7c44028cdfaade19f815
Dmitry Balahonov <balahonov.dmitry@gmail.com>
2020-02-22 01:55:39 +0300

    refactor: change algorithm dream-team task.

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,12 +1,13 @@
 module.exports = function createDreamTeam(members) {
-    let array = [];
-    let array2 = [];
-    for (let i in members) {
-        if (typeof members[i] === 'string') {
-          array.push(members[i].trim()[0]);
-        } else {
-          array2.push(members[i]);
+    if (!members || typeof members === 'undefined'){
+        return false;
+    }
+    const arr = Array.from(members);
+    let result = [];
+    for (let i = 0; i < arr.length; i++){
+        if (typeof arr[i] === 'string'){
+            result.push(arr[i].trim()[0].toLocaleUpperCase());
         }
     }
-    return array.sort().join("").toLocaleUpperCase();
+    return result.sort().join('').toLocaleUpperCase();
 };
